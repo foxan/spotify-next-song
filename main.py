3ce068cea20c3ae7bc90b56790a5166491c70604
Foxan Ng <foxan.ng@gmail.com>
2020-12-20 12:06:40 +0900

    Sleep for a random number of seconds within the range
    So that multiple playback check will not skip track at the same time and cause unexpected behaviors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import configparser
+import random
 import spotipy
 import time
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -7,7 +8,7 @@ from datetime import timedelta
 from spotipy.oauth2 import SpotifyOAuth
 
 # time left before skipping a song (in seconds)
-skip_threshold = 3
+skip_threshold = 7
 
 config = configparser.ConfigParser()
 config.read_file(open("config.ini"))
@@ -29,11 +30,13 @@ def get_current_playback():
             time_left = (results["item"]["duration_ms"] - results["progress_ms"]) // 1000
             artist_name = results["item"]["artists"][0]["name"]
             song_name = results["item"]["name"]
-            print(f'Now playing: {artist_name} - {song_name}, Time left: {timedelta(seconds=time_left)}')
+            print(f'Now playing: {artist_name} - {song_name}, Time left: {timedelta(seconds=time_left)}, ', end="")
 
             # wait till song is expected to end in skip_threshold
             if time_left - skip_threshold > 0:
-                time.sleep(time_left - skip_threshold)
+                sleep_for = time_left - round(random.uniform(skip_threshold - 4, skip_threshold), 2)
+                print(f'Sleep for: {str(timedelta(seconds=sleep_for)).split("0000")[0]}')
+                time.sleep(sleep_for)
 
             # need to check again as there may be actions (pause, skip, etc) in between
             results = sp.current_playback()
