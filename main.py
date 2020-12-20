4d4d653bb2cb9e453d16a8b2669481e00a1ee83e
Foxan Ng <foxan.ng@gmail.com>
2020-12-20 11:10:41 +0900

    First working version using AsyncIOScheduler

---
+++ main.py
@@ -0,0 +1,54 @@
+import asyncio
+import configparser
+import spotipy
+import time
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import timedelta
+from spotipy.oauth2 import SpotifyOAuth
+
+# time left before skipping a song (in seconds)
+skip_threshold = 3
+
+config = configparser.ConfigParser()
+config.read_file(open("config.ini"))
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config["DEFAULT"]["client_id"],
+                                               client_secret=config["DEFAULT"]["client_secret"],
+                                               redirect_uri="http://localhost:9000/callback/",
+                                               scope="user-read-playback-state user-modify-playback-state"),
+                     # specify Spotify to respond in Chinese
+                     language="zh-TW")
+
+
+def get_current_playback():
+    """ get current song info, sleep for (time left - skip threshold) seconds, then skip song """
+    results = sp.current_playback()
+    if results is not None:
+        # only proceed if song is played on Cast devices
+        if results["device"]["type"] in ("CastVideo", "CastAudio"):
+            time_left = (results["item"]["duration_ms"] - results["progress_ms"]) // 1000
+            artist_name = results["item"]["artists"][0]["name"]
+            song_name = results["item"]["name"]
+            print(f'Now playing: {artist_name} - {song_name}, Time left: {timedelta(seconds=time_left)}')
+
+            # wait till song is expected to end in skip_threshold
+            if time_left - skip_threshold > 0:
+                time.sleep(time_left - skip_threshold)
+
+            # need to check again as there may be actions (pause, skip, etc) in between
+            results = sp.current_playback()
+            time_left = (results["item"]["duration_ms"] - results["progress_ms"]) // 1000
+            if time_left <= skip_threshold:
+                sp.next_track()
+                print(f'Time left: {time_left}s, song skipped')
+
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(get_current_playback, "interval", seconds=60, max_instances=5)
+scheduler.start()
+
+if __name__ == "__main__":
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        pass
